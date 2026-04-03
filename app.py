4eeed813f3ca1424a694d38726bad34fa2627af6
rank1dave <addo.david32@gmail.com>
2026-04-02 20:10:11 -0500

    add customer profile generator

---
+++ app.py
@@ -0,0 +1,40 @@
+from decimal import Decimal
+import math
+print(Decimal('0.1') + Decimal('0.2'))
+course = """yo it's david famm"""
+print(len("""yo it's david famm"""))
+print(course[-1])
+print(course[0:5])
+f = 'david'
+l = 'addo'
+print(f"{f} {l}")
+
+print(f.capitalize())
+
+print('ddo' not in f)
+if 'd' in f:
+    print("he's in there")
+
+print(10 / 3)
+
+print(10 // 3)
+
+print(10 % 3)
+
+print(10 ** 3)
+
+x = 4
+x = x + 6
+
+x += 9
+print(x)
+
+print(math.factorial(8))
+
+# i = input("i: ")
+# y = int(i) + 8
+# print(f"i: {i}, y: {y}")
+
+print('HEYOO gang'.capitalize())
+
+print(f"{f} \t hey")
